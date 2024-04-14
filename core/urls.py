caa6ef175d7e9856f0ede43db6de93ac518442c1
LE THI HANG <lethihang111001@gmail.com>
2024-04-15 01:37:38 +0700

    confict

--- core/urls.py
+++ core/urls.py
@@ -3,14 +3,10 @@ from paypal.standard.ipn import views as paypal_ipn_views
 from core import views
 from core.views import intro, index, category_list_view, product_list_view, category_product_list_view, \
     product_detail_view, ajax_add_review, add_to_cart, cart_view, delete_item_from_cart, update_cart, checkout_view, \
-<<<<<<< HEAD
     payment_completed_view, payment_failed_view, customer_dashboard, order_detail, make_address_default, wishlist_view, \
     add_to_wishlist, remove_wishlist
 from core import glass_virtual_tryon
 from core.glass_virtual_tryon import test_view
-=======
-    payment_completed_view, payment_failed_view,glass_try,customer_dashboard
->>>>>>> 43074547d84c9b41f1ef93325009163f0499c7ba
 
 app_name = "core"
 
@@ -41,7 +37,6 @@ urlpatterns = [
     path("payment-completed/",payment_completed_view, name="payment-completed"),
     #payment fail
     path("payment-failed/",payment_failed_view, name="payment-failed"),
-<<<<<<< HEAD
     path("test/",test_view, name="test"),
     path("dashboard/",customer_dashboard, name="dashboard"),
     path("dashboard/order/<int:id>",order_detail, name="order-detail"),
@@ -49,12 +44,5 @@ urlpatterns = [
     path("wishlist/",wishlist_view, name="wishlist"),
     path("add-to-wishlist/",add_to_wishlist, name="add-to-wishlist"),
     path("remove-from-wishlist/",remove_wishlist, name="remove-from-wishlist"),
-
-=======
-    path("glass_try/", glass_try, name="glass_try"),
-    # dashboard
-    path("dashboard/", customer_dashboard, name="dashboard"),
->>>>>>> 43074547d84c9b41f1ef93325009163f0499c7ba
-
 ]
 
